281155bb40540fcb127357191a4074e361000e55
Josh Kasitz <jmkasitz@uark.edu>
2020-03-31 23:17:00 -0500

    Sprint 4 Init commit Josh Kasitz

---
+++ src/main/java/edu/uark/registerapp/controllers/TransactionRouteController.java
@@ -0,0 +1,44 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
+import edu.uark.registerapp.models.entities.ActiveUserEntity;
+import edu.uark.registerapp.models.enums.EmployeeClassification;
+
+@Controller
+@RequestMapping(value = "/transactionDetails")
+public class TransactionRouteController extends BaseRouteController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView start(
+		@RequestParam final Map<String, String> queryParameters,
+		final HttpServletRequest request
+	) {
+        // Needs to be altered for transaction JK
+        final ModelAndView modelAndView =
+			this.setErrorMessageFromQueryString(
+				new ModelAndView(ViewNames.PRODUCT_DETAIL.getViewName()),
+				queryParameters);
+		return modelAndView;
+	}
+
+	// Properties
+	//@Autowired
+	//private ProductQuery productQuery;
+}
